ba99a90eed48114a593f6c434f7649167dc9f4ee
swasilyev <swasilyev@gmail.com>
2020-12-05 21:40:43 +0300

    benches added

--- mipp/src/lib.rs
+++ mipp/src/lib.rs
@@ -28,13 +28,14 @@ mod tests {
     use ark_ec::ProjectiveCurve;
     use ark_ff::{One, test_rng, UniformRand, Zero};
     use ark_inner_products::{InnerProduct, PairingInnerProduct};
+    use bench_utils::{end_timer, start_timer};
     use rand::Rng;
 
     use super::*;
 
     #[test]
     fn mipp_srs() {
-        let n = 16;
+        let n = 1024;
 
         let rng = &mut test_rng();
 
@@ -46,18 +47,24 @@ mod tests {
             .collect::<Vec<_>>();
         let apk = IP::inner_product(&pks, &bitmask).unwrap();
 
+        let setup = start_timer!(|| "MIPP_SRS::setup");
         let (srs, _) = MIPP_SRS::setup(rng, n).unwrap();
+        end_timer!(setup);
+
         let (ck, _) = srs.get_commitment_keys();
         let v_srs = srs.get_verifier_key();
 
         let com_pks= PairingInnerProduct::<Bls12_381>::inner_product(&pks, &ck).unwrap();
 
+        let prove = start_timer!(|| "MIPP_SRS::prove_with_structured_scalar_message");
         let proof = MIPP_SRS::prove_with_structured_scalar_message(
             &srs,
             (&pks, &bitmask),
             (&ck, &HomomorphicPlaceholderValue),
         ).unwrap();
+        end_timer!(prove);
 
+        let verify = start_timer!(|| "MIPP_SRS::verify_with_unfolded_right_message");
         let proof_valid = MIPP_SRS::verify_with_unfolded_right_message(
             &v_srs,
             &HomomorphicPlaceholderValue,
@@ -65,13 +72,14 @@ mod tests {
             bitmask,
             &proof,
         ).unwrap();
+        end_timer!(verify);
 
         assert!(proof_valid);
     }
 
     #[test]
     fn tipa_kzg() {
-        let n = 16;
+        let n = 1024;
 
         let rng = &mut test_rng();
 
@@ -83,7 +91,10 @@ mod tests {
             .collect::<Vec<_>>();
         let apk = IP::inner_product(&pks, &bitmask).unwrap();
 
+        let setup = start_timer!(|| "TIPA_KZG::setup");
         let (srs, ck_t) = TIPA_KZG::setup(rng, n).unwrap();
+        end_timer!(setup);
+
         let (ck_pks, ck_bitmask) = srs.get_commitment_keys();
         let v_srs = srs.get_verifier_key();
 
@@ -91,9 +102,13 @@ mod tests {
         let com_bitmask = SC1::commit(&ck_bitmask, &bitmask).unwrap();
         let com_t = IPC::commit(&vec![ck_t.clone()], &vec![apk]).unwrap();
 
+        let prove = start_timer!(|| "TIPA_KZG::prove");
         let proof = TIPA_KZG::prove(&srs, (&pks, &bitmask), (&ck_pks, &ck_bitmask, &ck_t)).unwrap();
+        end_timer!(prove);
 
+        let verify = start_timer!(|| "TIPA_KZG::verify");
         let proof_valid = TIPA_KZG::verify(&v_srs, &ck_t, (&com_pks, &com_bitmask, &com_t), &proof).unwrap();
+        end_timer!(verify);
 
         assert!(proof_valid);
     }
