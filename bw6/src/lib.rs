445b09b744fb3e9f12d57e812bf6e6bed43447a3
swasilyev <swasilyev@gmail.com>
2021-01-25 00:56:53 +0300

    Prover is a struct holding preprocessed transcript and explicit list of public keys

--- bw6/src/lib.rs
+++ bw6/src/lib.rs
@@ -9,7 +9,9 @@ pub use self::verifier::*;
 pub mod endo;
 pub mod utils;
 pub mod bls;
+
 pub use bls::{Signature, SecretKey, PublicKey};
+
 mod transcript;
 
 use ark_ff::{One, Field, batch_inversion};
@@ -65,7 +67,7 @@ impl PreparedVerifierKey {
             zeta_n.square_in_place();
         }
         assert_eq!(zeta_n, zeta.pow([self.domain_size]));
-        let zeta_n_minus_one= zeta_n - F::one();
+        let zeta_n_minus_one = zeta_n - F::one();
         let zeta_n_minus_one_div_n = zeta_n_minus_one * self.domain.size_inv;
 
         let mut inv = [zeta - F::one(), self.domain.group_gen * zeta - F::one()];
@@ -85,7 +87,7 @@ impl Params {
         let n = domain.size();
 
         // deg(q) = 3n-3
-        let kzg_params = KZG_BW6::setup(3*n-2, false, rng).unwrap();
+        let kzg_params = KZG_BW6::setup(3 * n - 2, false, rng).unwrap();
 
         Self {
             domain,
@@ -107,9 +109,9 @@ impl Params {
         let n = self.domain.size();
         ProverKey {
             domain_size: n,
-            kzg_ck: self.get_ck(3*n-2),
+            kzg_ck: self.get_ck(3 * n - 2),
             domain: self.domain,
-            h: self.h
+            h: self.h,
         }
     }
 
@@ -157,9 +159,9 @@ impl SignerSet {
         assert!(num_pks > 1); // https://github.com/arkworks-rs/poly-commit/issues/40
         Self(
             (0..num_pks)
-            .map(|_| SecretKey::new(rng))
-            .map(|sk| PublicKey::from(&sk))
-            .collect::<Vec<_>>()
+                .map(|_| SecretKey::new(rng))
+                .map(|sk| PublicKey::from(&sk))
+                .collect::<Vec<_>>()
         )
     }
 
@@ -226,6 +228,9 @@ mod tests {
         let (pks_x_comm, pks_y_comm) = signer_set.commit(&params.get_ck(pks_domain_size));
         println!("{}μs = signer set commitment", signer_set_commitment.elapsed().as_micros());
 
+        let prover = Prover::new(params.to_pk(), &pks_x_comm, &pks_y_comm, signer_set.get_all(), Transcript::new(b"apk_proof"));
+        let verifier = Verifier::new(params.to_vk(), pks_x_comm, pks_y_comm, Transcript::new(b"apk_proof"));
+
         let mut transcript = Transcript::new(b"apk_proof");
         // transcript.set_protocol_params();
         transcript.set_signer_set(&pks_x_comm, &pks_y_comm); //TODO: size
@@ -235,7 +240,7 @@ mod tests {
 
         // let proving = Instant::now();
         let prove_ = start_timer!(|| "BW6 prove");
-        let proof = prove(&b, signer_set.get_all(), &params.to_pk(), &mut transcript);
+        let proof = prover.prove(&b);
         end_timer!(prove_);
         // println!("{}μs = proving\n", proving.elapsed().as_micros());
         let mut serialized_proof = vec![0; proof.serialized_size()];
@@ -243,7 +248,6 @@ mod tests {
 
         let proof = Proof::deserialize(&serialized_proof[..]).unwrap();
 
-        let verifier = Verifier::new(params.to_vk(), pks_x_comm, pks_y_comm, Transcript::new(b"apk_proof"));
         // let verification = Instant::now();
         let verify_ = start_timer!(|| "BW6 verify");
         let valid = verifier.verify(&apk, &b, &proof);
@@ -257,14 +261,14 @@ mod tests {
     fn test_lagrange_evaluations() {
         let n = 16;
         let rng = &mut test_rng();
-        let params = Params::new(n-1, rng);
+        let params = Params::new(n - 1, rng);
         assert_eq!(params.domain.size(), n);
 
         let z = F::rand(rng);
         let evals = params.to_vk().lagrange_evaluations(z);
         assert_eq!(evals.vanishing_polynomial, params.domain.evaluate_vanishing_polynomial(z));
-        let coeffs =  params.domain.evaluate_all_lagrange_coefficients(z);
+        let coeffs = params.domain.evaluate_all_lagrange_coefficients(z);
         assert_eq!(evals.l_0, coeffs[0]);
-        assert_eq!(evals.l_minus_1, coeffs[n-1]);
+        assert_eq!(evals.l_minus_1, coeffs[n - 1]);
     }
 }
